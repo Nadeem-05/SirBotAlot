2a314bc7316913c69786778f032962d3746b87fc
Nadeem <m.nadeem83005@gmail.com>
2022-12-20 22:41:45 +0530

    fix again

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ async def load_extensions():
         if filename.endswith(".py"):
             # cut off the .py from the file name
             await bot.load_extension(f"cogs.{filename[:-3]}")
+            await bot.start("ODA3OTI2MjA5MDQ0MjgzNDIy.GXtHXK.oDMgkykilcVIl5C2j0yfag9_nB8OSTjUtU4hi0")
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-bot.run("ODA3OTI2MjA5MDQ0MjgzNDIy.GXtHXK.oDMgkykilcVIl5C2j0yfag9_nB8OSTjUtU4hi0")
+
