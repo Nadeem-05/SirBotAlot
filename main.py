1ae9397e4a34d306a18aaeabeeec04a1a70d0386
Nadeem M <80162515+Nadeem-05@users.noreply.github.com>
2022-12-20 22:01:25 +0530

    Bruh

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ intents.message_content=True
 bot = commands.Bot(command_prefix=">", intents=intents)
 version = 1
 bot.remove_command('help')
-with open('token.txt', 'r') as fp:
-    TOKEN = fp.read().rstrip()
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
